a3606240be61f965b2025d7aa68888b174209595
Nidhish Lakhinana <47395841+nidhish1602@users.noreply.github.com>
2019-08-31 12:59:04 +0530

    Add files via upload

---
+++ book.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class book {
+	String name;
+	String usn;
+	String author;
+	int n_pages;
+	
+	
+	public static void main(String args[])
+	{
+		int n;
+		Scanner s= new Scanner(System.in);
+		System.out.println("Enter the number of books:");
+		n=s.nextInt();
+		
+	}
+	
+}
