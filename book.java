06c3a57dd25197a55b2ce67299c7a27568c12902
Nidhish Lakhinana <47395841+nidhish1602@users.noreply.github.com>
2019-09-08 20:36:12 +0530

    Update book.java

--- book.java
+++ book.java
@@ -1,19 +1,45 @@
 import java.util.*;
 
-public class book {
-	String name;
-	String usn;
-	String author;
-	int n_pages;
-	
-	
-	public static void main(String args[])
-	{
-		int n;
-		Scanner s= new Scanner(System.in);
-		System.out.println("Enter the number of books:");
-		n=s.nextInt();
-		
-	}
-	
+class book
+{
+String name ;
+String author;
+int price;
+int num_pages;
+public void book()
+{
+name = "" ;
+author = "";
+price = 0;
+num_pages = 0;
+}
+
+public void get()
+{
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter the details :");
+System.out.println("Enter the name of the Book");
+name = sc.nextLine();
+System.out.println("Enter the name of the Author");
+author = sc.nextLine();
+System.out.println("Enter the price of the Book");
+price = sc.nextInt();
+System.out.println("Enter the total number of pages");
+num_pages = sc.nextInt();
+}
+
+public String toString()
+{
+return "\n" + "Name : " + name + " \n" + "Author : " + author + " \n" + "Price : " + price + " \n" + "Total number of pages : " + num_pages;
+}
+
+public static void main(String args[])
+{
+book b = new book();
+
+b.get();
+System.out.println(b);
+
+
 }
